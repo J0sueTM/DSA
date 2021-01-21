88edba40cbe66847d2631e914541f33940726818
J0sueTM <jteodomo@gmail.com>
2021-01-21 20:09:05 -0300

    Add SingleLinkedList for c

---
+++ Implementation/DataStructures/c/SingleLinkedList.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct linked_list_node {
+  int data;
+  struct linked_list_node *next_linked_list_node;
+};
+
+struct linked_list_node *insert_node_at_start(struct linked_list_node **root_linked_list_node, int new_node_data) {
+  if (!(*root_linked_list_node))
+    return NULL;
+
+  struct linked_list_node *new_linked_list_node = (struct linked_list_node *)malloc(sizeof(struct linked_list_node));
+  new_linked_list_node->data = new_node_data;
+  new_linked_list_node->next_linked_list_node = (*root_linked_list_node)->next_linked_list_node; /* expected to be NULL */
+  (*root_linked_list_node)->next_linked_list_node = new_linked_list_node;
+
+  return new_linked_list_node;
+}
+
+struct linked_list_node *insert_node_at_middle(struct linked_list_node **root_linked_list_node, int prev_node_to_insert_key, int new_node_data) {
+  if (!(*root_linked_list_node))
+    return NULL;
+
+  struct linked_list_node *temp_linked_list_node = (*root_linked_list_node)->next_linked_list_node;
+  while (temp_linked_list_node != NULL || temp_linked_list_node->data != prev_node_to_insert_key)
+    temp_linked_list_node = temp_linked_list_node->next_linked_list_node; 
+
+  if (!temp_linked_list_node)
+    return NULL;
+
+  struct linked_list_node *new_linked_list_node = (struct linked_list_node *)malloc(sizeof(struct linked_list_node));
+  new_linked_list_node->data = new_node_data;
+
+  new_linked_list_node->next_linked_list_node = temp_linked_list_node->next_linked_list_node;
+  temp_linked_list_node->next_linked_list_node = new_linked_list_node;
+
+  return new_linked_list_node;
+}
+
+struct linked_list_node *insert_node_at_end(struct linked_list_node **root_linked_list_node, int new_node_data) {
+  if (!(*root_linked_list_node))
+    return NULL;
+
+  struct linked_list_node *temp_linked_list_node = *root_linked_list_node;
+  while (temp_linked_list_node->next_linked_list_node != NULL)
+    temp_linked_list_node = temp_linked_list_node->next_linked_list_node;
+
+  struct linked_list_node *new_linked_list_node = (struct linked_list_node *)malloc(sizeof(struct linked_list_node));
+  new_linked_list_node->data = new_node_data;
+  new_linked_list_node->next_linked_list_node = temp_linked_list_node->next_linked_list_node; /* expected to be NULL */
+  temp_linked_list_node->next_linked_list_node = new_linked_list_node;
+
+  return new_linked_list_node;
+}
+
+int remove_node_at_key(struct linked_list_node **root_linked_list_node, int node_to_delete_key) {
+  if (!(*root_linked_list_node))
+    return INT_MIN;
+
+  struct linked_list_node *temp_linked_list_node = *root_linked_list_node;
+  while (temp_linked_list_node->next_linked_list_node != NULL || temp_linked_list_node->data != node_to_delete_key)
+    temp_linked_list_node = temp_linked_list_node->next_linked_list_node;
+
+  if (temp_linked_list_node->data != node_to_delete_key || temp_linked_list_node == (*root_linked_list_node))
+    return INT_MIN;
+
+  free(temp_linked_list_node);
+}
+
+void display_linked_list(struct linked_list_node **root_linked_list_node) {
+  struct linked_list_node *temp_linked_list_node = (*root_linked_list_node)->next_linked_list_node;
+  for (int i = 0; temp_linked_list_node != NULL; ++i) {
+    printf("Position %d: %d", i, temp_linked_list_node->data);
+
+    temp_linked_list_node = temp_linked_list_node->next_linked_list_node;
+  }  
+}
+
+void end_linked_list(struct linked_list_node **root_linked_list_node) {
+  struct linked_list_node *temp_linked_list_node;
+  temp_linked_list_node = (*root_linked_list_node)->next_linked_list_node;
+  
+  while ((*root_linked_list_node)->next_linked_list_node != NULL) {
+    temp_linked_list_node = temp_linked_list_node->next_linked_list_node; 
+    free((*root_linked_list_node)->next_linked_list_node);
+
+    (*root_linked_list_node)->next_linked_list_node = temp_linked_list_node;
+  }
+
+  free(*root_linked_list_node);
+}
+
+int main(void) {
+  struct linked_list_node *root_node = (struct linked_list_node *)malloc(sizeof(struct linked_list_node));
+  root_node->data = 0;
+  root_node->next_linked_list_node = NULL;
+
+  insert_node_at_start(&root_node, 1);
+  insert_node_at_end(&root_node, 3);
+  printf("%d\n", insert_node_at_middle(&root_node, 1, 2)->data);
+
+  end_linked_list(&root_node);
+
+  return 0;
+}
